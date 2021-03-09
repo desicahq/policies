d3944f61e2323dc365f9fa46079fec46b7f8eeef
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-03-09 11:05:59 +0000

    Urls and favicon

--- pages/index.md
+++ pages/index.md
@@ -2,6 +2,6 @@
 
 This is the Desica leagal hub, where you can find all of our leagal policies and infomation. At Desica, we understand that you are entrusting us to protect your privacy, and we believe that your data is none of our business. That's why we never sell, rent or share your data with anyone.
 
-<img src="contract.png" class="home-img"></img>
+<img src="contract.png" className="home-img"></img>
 
 In some cases, our sevices have seperate policies that apply to them. Desiads is an example of such a product, and as you can see, there is a folder titled Desiads, where you can find the product specific polices that apply on Desiads.
--- theme.config.js
+++ theme.config.js
@@ -18,17 +18,14 @@ export default {
       <meta name="description" content="Nextra: the next site builder" />
       <meta name="og:description" content="Nextra: the next site builder" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="twitter:image" content="https://policies.desica.uk/og.png" />
+      <meta name="twitter:site:domain" content="policies.desica.uk" />
+      <meta name="twitter:url" content="https://policies.desica.uk" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="og:image" content="https://policies.desica.uk/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-      <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="icon" type="image/png" href="https://static.desica.uk/hotlink-ok/favicon.ico"/>
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
     </>
   ),
